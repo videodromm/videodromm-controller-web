35ea786456543dfa0032c4271624e938ad82ab7e
Bruce LANE <bruce@batchass.fr>
2016-12-05 15:33:42 +0100

    uniforms data

--- process/js/app.js
+++ process/js/app.js
@@ -5,6 +5,18 @@ var MainInterface = React.createClass({
   getInitialState: function() {
     return {
       title: 'Uniforms',
+      data: [
+        {
+          "uniformName": "iZoom",
+          "uniformInfo": "zoom level -3.0 to 3.0",
+          "uniformValue": 0.5
+        },
+        {
+          "uniformName": "iExposure",
+          "uniformInfo": "exposure 0.0 to 3.0",
+          "uniformValue": 1.5
+        }
+      ],
       show: true
     } //return
   }, //getInitialState
@@ -14,9 +26,18 @@ var MainInterface = React.createClass({
     return (
       <div className="interface">
         <h1>{this.state.title}</h1>
-        
+        <div className="item-list media-list">
+          <ul className="item-list media-list">
+            <li className="uniform-info media-body">
+              <div className="uniform-head">
+                <span className="uniform-name">{this.state.data[0].uniformName}</span>
+                <span className="uniform-info pull-right">{this.state.data[0].uniformInfo}</span>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-    )
+    ) // return
   } //render
 }); //MainInterface
 
